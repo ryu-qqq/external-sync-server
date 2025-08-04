c59cd2c9daa9fb2fa5780e4a99abd6dedf75a1c9
RyuSangwon <rsw2@connectly.co.kr>
2025-08-04 14:52:43 +0900

    first commit

---
+++ src/test/java/com/ryuqq/externalsyncserver/ExternalSyncServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryuqq.externalsyncserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalSyncServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
